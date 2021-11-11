48dc2d0434b33835623645da7e9cff28a793d652
Lopez1710 <69539735+Lopez1710@users.noreply.github.com>
2021-11-10 20:55:48 -0600

    Registro

---
+++ Lofi_Spot/Lofi_Spot/Controllers/UsuarioController.cs
@@ -0,0 +1,35 @@
+﻿using Lofi_Spot.Dominio;
+using Lofi_Spot.Service;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lofi_Spot.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private IUsuarios iusuario;
+        public UsuarioController(IUsuarios iusuario)
+        {
+            this.iusuario = iusuario;
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Registro(Usuarios usuario)
+        {
+            Usuarios us = new Usuarios();
+            us.Nick = usuario.Nick;
+            us.Email = usuario.Email;
+            us.Pass = usuario.Pass;
+            us.RolID = 2;
+            iusuario.Insert(us);
+            return View();
+        }
+    }
+}
